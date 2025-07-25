867c53e89306cdc002e8cba0479654033b02eca7
MD. SAHIL <codewithkinu@gmail.com>
2025-07-25 18:39:24 +0530

    feat :collaption fixed in resume and article

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,41 +1,31 @@
-import React, { useState } from 'react';
-import { Edit, Sparkles } from 'lucide-react';
+import { useState } from 'react';
+import { Eraser, FileText, Sparkles } from 'lucide-react';
 import axios from 'axios';
 import { useAuth } from '@clerk/clerk-react';
 import toast from 'react-hot-toast';
-import Markdown from 'react-markdown'
-
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+import Markdown from 'react-markdown';
 
-const WriteArticle = () => {
-  const articleLength = [
-    { length: 800, text: 'Short (500-800 words)' },
-    { length: 1200, text: 'Medium (800-1200 words)' },
-    { length: 1600, text: 'Long (1200+ words)' },
-  ];
-
-  const [selectedLength, setSelectedLength] = useState(articleLength[0]);
+const ReviewResume = () => {
   const [input, setInput] = useState('');
   const [content, setContent] = useState('');
   const [loading, setLoading] = useState(false);
   const { getToken } = useAuth();
-
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const prompt = `Write an article about ${input} in ${selectedLength.text}`;
-      const { data } = await axios.post(
-        '/api/ai/generate-article',
-        { prompt, length: selectedLength.length },
-        {
-          headers: { Authorization: `Bearer ${await getToken()}` },
-        }
-      );
+      const formData = new FormData();
+      formData.append('resume', input);
+      const { data } = await axios.post('/api/ai/resume-review', formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+
       if (data.success) {
         setContent(data.content);
       } else {
-        alert("data not recieved")
         toast.error(data.message);
       }
     } catch (error) {
@@ -43,78 +33,59 @@ const WriteArticle = () => {
     }
     setLoading(false);
   };
-
   return (
     <div className="h-full overflow-y-scroll p-6 flex flex-col lg:flex-row items-start gap-4 text-slate-700">
-      {/* Form Section */}
       <form
         onSubmit={onSubmitHandler}
         className="w-full lg:w-1/2 p-4 bg-white rounded-lg border border-gray-200"
       >
         <div className="flex items-center gap-3">
-          <Sparkles className="w-6 text-[#4a7aff]" />
-          <h1 className="text-xl font-semibold">Article Configuration</h1>
+          <Sparkles className="w-6 text-[#00da83]" />
+          <h1 className="text-xl font-semibold">Resume Review</h1>
         </div>
 
-        <p className="mt-6 text-sm font-medium">Article Topic</p>
+        <p className="mt-6 text-sm font-medium">Upload Resume</p>
         <input
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          type="text"
-          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
-          placeholder="The future of AI is...."
+          onChange={(e) => setInput(e.target.files[0])}
+          accept="application/pdf"
+          type="file"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
           required
         />
-
-        <p className="mt-4 text-sm font-medium">Article Length</p>
-        <div className="mt-3 flex gap-3 flex-wrap">
-          {articleLength.map((item, index) => (
-            <span
-              onClick={() => setSelectedLength(item)}
-              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
-                selectedLength.text === item.text
-                  ? 'bg-blue-50 text-blue-700'
-                  : 'text-gray-500 border-gray-300'
-              }`}
-              key={index}
-            >
-              {item.text}
-            </span>
-          ))}
-        </div>
-        <br />
+        <p className="text-xs text-gray-500 font-light mt-1">
+          Supports pdf resume only
+        </p>
         <button
           type="submit"
           disabled={loading}
-          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226bff] to-[#65adff] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00da83] to-[#009bb3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
         >
           {loading ? (
             <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
           ) : (
-            <Edit className="w-5" />
+            <FileText className="w-5" />
           )}
-          Generate Article
+          Review Resume
         </button>
       </form>
 
-      {/* Generated Article Section */}
       <div className="w-full lg:w-1/2 p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
         <div className="flex items-center gap-3">
-          <Edit className="w-5 h-5 text-[#4a7aff]" />
-          <h1 className="text-xl font-semibold">Generated Article</h1>
+          <FileText className="w-5 h-5 text-[#00da83]" />
+          <h1 className="text-xl font-semibold">Analysis Results</h1>
         </div>
 
         {!content ? (
           <div className="flex-1 flex justify-center items-center">
             <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-              <Edit className="w-9 h-9" />
-              <p>Enter a topic and click "Generate Article" to get started</p>
+              <FileText className="w-9 h-9" />
+              <p>Upload a resume and click "Review Resume" to get started</p>
             </div>
           </div>
         ) : (
           <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
-            <div className='reset-tw'>
-              <Markdown >{content}</Markdown>
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
             </div>
           </div>
         )}
@@ -123,4 +94,4 @@ const WriteArticle = () => {
   );
 };
 
-export default WriteArticle;
+export default ReviewResume;
