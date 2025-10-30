9f23333b274810e6935c7a7a4245482daba5b86e
MD. SAHIL <codewithkinu@gmail.com>
2025-10-30 14:30:25 +0530

    server with authentication using clerk and router set

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import { clerkMiddleware, requireAuth } from '@clerk/express';
+import aiRouter from './routes/aiRoutes.js';
+import userRouter from './routes/userRoutes.js';
+
+const app = express();
+await connectCloudinary();
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get('/', (req, res) => {
+  res.send('Serve is Live');
+});
+
+app.use((req, res, next) => {
+  console.log(`🚀 ${req.method} ${req.url}`);
+  next();
+});
+
+// app.use(requireAuth);
+
+app.use('/api/ai', aiRouter);
+app.use('/api/user', userRouter);
+
+const Port = process.env.PORT || 3000;
+app.listen(Port, () => {
+  console.log('server is running on port', Port);
+});
